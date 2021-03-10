f1f9c8cfe897594787bccb52c361d93af6e3d534
rafael1717y <rafaelufjf@gmail.com>
2021-03-10 15:26:46 -0300

    exemplo_scanf.c

---
+++ exemplo_scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+    int num;
+    char c;
+    scanf("%d %c", &num, &c);
+    printf("O número lido é: %d\nE o char lido é: %c\n", num, c);
+
+    
+
+    return 0;
+}
